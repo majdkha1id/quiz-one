a7073df5f8bcf3b52e2862c01d1f5d4266eb9a5b
majdkha1id <122222965+majdkha1id@users.noreply.github.com>
2024-02-20 22:48:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// QUIZ 1
+
+let no1 = 10
+let no2 = 20
+let no3 = 5
+if(no1 > no2 && no1 > no3){
+    console.log(no1)
+}
+else if(no2 > no1 && no2 > no3){
+    console.log(no2)
+}
+else{
+    console.log(no3)
+}
+
+
+
+
